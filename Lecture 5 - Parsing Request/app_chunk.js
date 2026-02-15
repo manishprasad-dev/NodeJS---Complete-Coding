c846643ee2f09bda088d20990def32f513547cba
Manish Kumar Prasad <mkak.2530@gmail.com>
2026-02-15 17:07:43 +0530

    basic done

--- Lecture 5 - Parsing Request/app_chunk.js
+++ Lecture 5 - Parsing Request/app_chunk.js
@@ -25,18 +25,24 @@ const server = http.createServer((req , res) => {
             return res.end();
         }else if(req.url === "/submit-details" && req.method == "POST"){
             // ! This time we will print the dynamic value entered by the user on the respective field
+
+
+            // Form data does NOT come at once.
+            // It comes as streamed chunks (Buffer format).
             const body = [];
             req.on('data', chunk=>{
                 console.log(chunk); // console log the chunk un buffer format
                 body.push(chunk);
             });
 
+            // This runs after ALL chunks are received. this whole is aysnchoronous
             req.on('end' , () =>{
-                const fullBody = Buffer.concat(body).toString();
+                const fullBody = Buffer.concat(body).toString(); //combining buffer and converting them into string
                 console.log(`Printing Raw : ${fullBody}`);
 
                 
-                const params = new URLSearchParams(fullBody);
+                const params = new URLSearchParams(fullBody); //Converting String to Object
+                //This is called URL encoded form data.
                 console.log(`Printing Params : ${params}`);
                 console.log(`Printing Params Entries : ${params.entries()}`);
                 const bodyObject = {};
@@ -45,6 +51,10 @@ const server = http.createServer((req , res) => {
                 }
 
                 console.log(bodyObject);
+
+                const jsonString = JSON.stringify(bodyObject);
+                console.log(`Printing JsonString : ${jsonString}`);
+                fs.writeFileSync("user-details" , jsonString);
             });
 
             // fs.writeFileSync("user-details.txt" , "Something Which Will Be Only Written If The Method Is POST");
